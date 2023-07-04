5162169e7768a906491a897dd1b0c61e7422ff78
Aditya <as23july@gmail.com>
2023-07-04 13:32:55 +0530

    updated on 4/07 props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
     <>
     <Navbar title = "Aditya Singh" about = "About"/>
+    <div className='container my-3'>
+      <TextForm heading="Enter text to analyse"/>
+    </div>
     </>
   );
 }
