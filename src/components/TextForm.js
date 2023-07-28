91ff8c2e791e56d89e8850adf7719cb75bdc3c2d
Aditya Singh <as23july@gmail.com>
2023-07-28 19:17:49 +0530

    disable button when no text are there

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,9 +30,9 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'black'}} onChange={handleOnChange} id="mybox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-3 my-2" onClick={handleUpClick}> Convert to Uppercase</button>
-                <button className="btn btn-primary mx-3 my-2" onClick={handleLoClick}> Convert to LowerCase</button>
-                <button className="btn btn-primary mx-3 my-2" onClick={clear}> Clear</button>
+                <button disabled= {text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleUpClick}> Convert to Uppercase</button>
+                <button disabled= {text.length === 0} className="btn btn-primary mx-3 my-2" onClick={handleLoClick}> Convert to LowerCase</button>
+                <button disabled= {text.length === 0} className="btn btn-primary mx-3 my-2" onClick={clear}> Clear</button>
             </div>
             <div className='container my-3' style={{color: props.mode === 'dark'?'white':'black'}}>
             <h1> Your Text Summary</h1>
