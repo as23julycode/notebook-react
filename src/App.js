0497dd2f19137457ad86f8b2c0ed06257cc9e5d4
Aditya Singh <as23july@gmail.com>
2023-08-10 08:32:48 +0530

    node server setup

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  
-  const toggleMode = () => {
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#282854';
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-    }
-  }
   return (
-    <>
-    <Router>
-    <Navbar firstcolumn = "Aditya Singh" secondcolumn = "Project" thirdcolumn = "About Me(Aditya Singh)"  mode={mode} toggleMode={toggleMode}/>
-
-    <div className='container my-3'>
-        <Routes>
-          <Route path="/about" element={<About mode = {mode}/>}/>
-          <Route path="/" element = {<TextForm heading="Enter text to analyse" mode={mode}/>}/>
-        </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </Router>
-    </>
   );
 }
 
