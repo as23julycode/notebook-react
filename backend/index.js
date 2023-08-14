01357c9a7523c9ca94717490f11707ceb67f221a
Aditya Singh <as23july@gmail.com>
2023-08-14 11:09:00 +0530

    api created

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,14 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World this is h good!')
+app.get('/api/sign', (req, res) => {
+  res.send('Hello World this is sign up')
+})
+app.get('/api/v1', (req, res) => {
+  res.send('Hello World this is viesion 1')
+})
+app.get('/api/v2', (req, res) => {
+  res.send('Hello World this version 2')
 })
 
 app.listen(port, () => {
