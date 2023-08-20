d7b266fbb7b81af1db7be029dc3d1fedf1514a4d
Aditya Singh <as23july@gmail.com>
2023-08-20 11:07:20 +0530

    password change to hath format and login token generated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'haiadityasingh';
 // create user using POST "/api/auth"
 router.post('/createuser',[
     body('name','enter valid name').isLength({ min : 3 }),
@@ -10,6 +13,7 @@ router.post('/createuser',[
     body('password').isLength({ min : 5 }),
 
     ], async(req, res) => {
+        // if there are errors then return bad request and error
     const error = validationResult(req);
     if (!error.isEmpty()) 
         {
@@ -24,16 +28,26 @@ router.post('/createuser',[
             {
                 return res.status(400).json({error: 'user with email Already registered'});
             }
+        const salt = await bcrypt.genSalt(10);
+        const secpassword = await bcrypt.hash(req.body.password, salt);
+            // creating new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secpassword,
             email: req.body.email,
-            })
-
-        res.json(user)
+            });
+        const data = {
+            user:{
+                id : user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        console.log(authtoken);
+        res.json(authtoken);
 
     } catch(error) {
         console.error(error.message)
+        res.status(500).send("some error occurred");
     }
 });
 
